fca9de31bc7c89316f59706fca0ddbac39c8dd10
Maxim Soolkovskiy <maximsoolkovskiy@gwsops.onmicrosoft.com>
2025-02-05 12:48:31 +0300

    Add project files.

---
+++ Core/PlaywrightConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using TAF.Core.Utils;
+using Microsoft.Playwright;
+using System.Text.Json.Serialization;
+
+namespace TAF.Core
+{
+    public class BrowserOptions
+    {
+
+        [JsonPropertyName("name")]
+        public string? Name { get; set; }
+        [JsonPropertyName("headed")]
+        public string? Headed { get; set; }
+    }
+
+    public class PlaywrightConfiguration
+    {
+        public readonly BrowserOptions browserOptions;
+        public readonly BrowserNewContextOptions browserNewContextOptions;
+
+        private class Config
+        {
+            [JsonInclude]
+            public BrowserOptions Browser { get; set; } = new BrowserOptions();
+            [JsonInclude]
+            public BrowserNewContextOptions BrowserContextOptions { get; set; } = new BrowserNewContextOptions();
+        }
+        public PlaywrightConfiguration()
+        {
+            var config = JsonReader.Read<Config>("config.json");
+            browserOptions = config.Browser;
+            browserNewContextOptions = config.BrowserContextOptions;
+        }
+    }
+}
---
+++ Core/TestConfiguration.cs
@@ -0,0 +1,12 @@
+﻿namespace PlaywrightSample.Core
+{
+    public class TestConfiguration
+    {
+        public string? TestUrl { get; set; }
+    }
+
+    public class TestOptions
+    {
+        public TestConfiguration? TestUrl { get; set; } = new TestConfiguration();
+    }
+}
